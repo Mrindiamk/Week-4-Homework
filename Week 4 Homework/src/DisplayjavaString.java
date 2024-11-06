789f8c1547f89b9e4fd462898118ad462af597fd
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-06 21:46:14 +0000

    fourth push

---
+++ Week 4 Homework/src/DisplayjavaString.java
@@ -0,0 +1,10 @@
+public class DisplayjavaString {
+    public static void main(String[] args) {
+
+        String xyz = "Aa kiu, I swd skieo";
+        int number = 236587 ;
+        String xyz2 = ". GH kiu: sieo??";
+
+        System.out.print(xyz + number + xyz2 + (25.33));
+    }
+}
