137be89df4ebb33e0fac1b5c9b59024ad84c9993
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-07 15:14:04 +0000

    my six push

---
+++ Week 4 Homework/src/AddNumber.java
@@ -0,0 +1,11 @@
+public class AddNumber {
+    public static void main(String[] args) {
+
+        int no1 = 5;
+        int no2 = (int)6.2;
+
+        System.out.println(no1);
+        System.out.println(no2+.2);
+
+    }
+}
