1273620966df7dfb6f956ab6bb7d431b6e446bed
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-07 15:07:33 +0000

    this is my fifth push

---
+++ Week 4 Homework/src/IntDoubleChar.java
@@ -0,0 +1,19 @@
+public class IntDoubleChar {
+    public static void main(String[] args) {
+      //integer
+        int no1= 10;
+        int no2= 2;
+
+        System.out.println(no1+no2);
+
+
+        //double
+        System.out.println("Hello Codebuster");
+
+
+        //character
+        char initial= 'K';
+        System.out.println(initial);
+
+    }
+}
